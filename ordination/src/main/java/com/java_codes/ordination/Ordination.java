eea6677deaba6425f7fef8ed2fff885ab5cf5f39
Rafael Sausen Bahr <rafaelsausen@gmail.com>
2024-02-13 15:18:32 -0300

    FEAT: Executing both ordinations

--- ordination/src/main/java/com/java_codes/ordination/Ordination.java
+++ ordination/src/main/java/com/java_codes/ordination/Ordination.java
@@ -1,8 +1,18 @@
 package com.java_codes.ordination;
 
+import java.util.Arrays;
+
 public class Ordination {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] aleatoryNumbers = { 5, 18, 10, 49, 16, 49, 10, 82, 43, 12 };
+
+        SelectionSort selectionSort = new SelectionSort();
+        int[] orderedNumbersSelection = selectionSort.execute(aleatoryNumbers);
+        System.out.println(Arrays.toString(orderedNumbersSelection));
+
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] orderedNumbersBubble = bubbleSort.execute(aleatoryNumbers);
+        System.out.println(Arrays.toString(orderedNumbersBubble));
     }
 }
